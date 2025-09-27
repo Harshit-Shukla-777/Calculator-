9a00093ebbeb0a51e7bbb635f46a5bf72b53dc71
Harshit Shukla <harshitshukla8587@gmail.com>
2025-09-27 13:23:50 +0530

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -22,6 +22,54 @@ elif operator == "/":
     
 else:
     print(f"{operator} isn't valid")
-    
+    #Python Calculator
+operator = input(" Enter Your Operator(+ - * /): ")
+
+num1 = float(input(" Enter the 1st number: "))
+num2 = float(input(" Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result , 5))
+
+elif operator == "-":
+    result = num1 - num2
+    print(round(result , 5))
+
+elif operator == "*":
+    result = num1 * num2
+    print(round(result , 5))
+
+elif operator == "/":
+    result = num1 / num2
+    print(round(result , 5))
+
+else:
+    print(f"{operator} isn't valid")
+#Python Calculator
+operator = input(" Enter Your Operator(+ - * /): ")
+
+num1 = float(input(" Enter the 1st number: "))
+num2 = float(input(" Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result , 5))
+
+elif operator == "-":
+    result = num1 - num2
+    print(round(result , 5))
+
+elif operator == "*":
+    result = num1 * num2
+    print(round(result , 5))
+
+elif operator == "/":
+    result = num1 / num2
+    print(round(result , 5))
+
+else:
+    print(f"{operator} isn't valid")
+
     
     
